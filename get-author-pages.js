aa079ba3e70196ef2bdd0a403e89c83fa362fdc2
Greg d'Eon <greg.l.deon@gmail.com>
2021-04-24 21:42:07 -0300

    Update TamperMonkey script

--- get-author-pages.js
+++ get-author-pages.js
@@ -13,18 +13,18 @@
     'use strict';
 
     // load URLs from localstorage; default to empty list if there are none
-	let url_list = JSON.parse(window.localStorage.getItem('url_list'))
-	if (url_list === null){
-		url_list = []
-	}
+    let url_list = JSON.parse(window.localStorage.getItem('url_list'))
+    if (url_list === null){
+        url_list = []
+    }
     console.log(url_list)
 
-	// add all authors on this page and save
-	let author_links = document.getElementsByClassName('gs_ai_name')
-	Array.prototype.forEach.call(author_links, el => {
-		let url = el.children[0].href
-		url_list.push(url)
-		console.log(url)
-	})
-	window.localStorage.setItem('url_list', JSON.stringify(url_list))
+    // add all authors on this page and save
+    let author_links = document.getElementsByClassName('gs_ai_name')
+    Array.prototype.forEach.call(author_links, el => {
+        let url = el.children[0].href
+        url_list.push(url)
+        console.log(url)
+    })
+    window.localStorage.setItem('url_list', JSON.stringify(url_list))
 })();
